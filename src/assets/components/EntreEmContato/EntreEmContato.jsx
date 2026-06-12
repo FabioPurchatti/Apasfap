07b7d14055f3ded302bac3c814fabe82f0f43533
FabioPurchatti <fabiopurchatti@gmail.com>
2026-06-12 09:53:22 -0300

    refactor: migrate EntreEmContato.jsx component to Tailwind CSS

--- src/assets/components/EntreEmContato/EntreEmContato.jsx
+++ src/assets/components/EntreEmContato/EntreEmContato.jsx
@@ -1,25 +1,25 @@
-import './EntreEmContato.css'
+
 
 function EntreEmContato() {
     return (
         <>
-            <section className="entre-em-contato">
+            <section className="flex justify-center flex-row p-12 items-center m-full bg-rosa-claro ">
                 <div>
-                    <h2>Entre em <span>Contato!</span></h2>
-                    <p>Entre em contato com a nossa equipe
+                    <h2 className="text-[70px] text-center font-black mr-80 leading-25  text-amarelo">Entre em <span className="flex flex-col justify-center items-center p-3">Contato!</span></h2>
+                    <p className="w-125 mr-20 ml-5 my-10 text-justify text-2xl leading-10 font-bold text-black ">Entre em contato com a nossa equipe
                         e saiba como apoiar a causa, esclarecer
                         dúvidas ou contribuir para que mais
                         animais recebam amor e cuidado.
                     </p>
                 </div>
-                <div className="container-formulario">
-                    <form className="formulario">
-                        <input type="text" id="nome" name="inNome" placeholder="Nome"></input>
-                        <input type="text" id="telefone" name="inTelefone" placeholder="Telefone"></input>
-                        <input type="text" id="email" name="inEmail" placeholder="E-mail"></input>
-                        <input type="text" id="assunto" name="inAssunto" placeholder="Assunto"></input>
-                        <textarea id="mensagem" name="inMensagem" placeholder="Mensagem"></textarea>
-                        <button>Enviar Mensagem</button>
+                <div className="flex flex-col items-center justify-center border-[3px] border-dashed border-marrom rounded-[40px] p-5 bg-amarelo ">
+                    <form className="flex flex-col justify-center items-center gap-6">
+                        <input className="font-bold bg-white text-marrom uppercase text-[20px] p-2 rounded-[5px] border-[3px] border-dashed border-marrom" type="text" id="nome" name="inNome" placeholder="Nome"></input>
+                        <input className="font-bold bg-white text-marrom uppercase text-[20px] p-2 rounded-[5px] border-[3px] border-dashed border-marrom" type="text" id="telefone" name="inTelefone" placeholder="Telefone"></input>
+                        <input className="font-bold bg-white text-marrom uppercase text-[20px] p-2 rounded-[5px] border-[3px] border-dashed border-marrom" type="text" id="email" name="inEmail" placeholder="E-mail"></input>
+                        <input className="font-bold bg-white text-marrom uppercase text-[20px] p-2 rounded-[5px] border-[3px] border-dashed border-marrom" type="text" id="assunto" name="inAssunto" placeholder="Assunto"></input>
+                        <textarea className="font-bold text-marrom uppercase text-[20px] p-2 bg-white rounded-[5px] border-[3px] border-dashed border-marrom w-77.5 h-35" id="mensagem" name="inMensagem" placeholder="Mensagem"></textarea>
+                        <button className="cursor-pointer bg-marrom text-[30px] uppercase font-bold text-barra-nav rounded-[10px] p-2">Enviar Mensagem</button>
                     </form>
                 </div>
             </section>
