71ee03ce5d2dcc7eda47cd3b815a42416fe8a045
FabioPurchatti <fabiopurchatti@gmail.com>
2026-06-18 11:02:47 -0300

    refactor: tuning components AceitamosTampinhas and JunteTampinhas

--- src/assets/components/AceitamosTampinhas/AceitamosTampinhas.jsx
+++ src/assets/components/AceitamosTampinhas/AceitamosTampinhas.jsx
@@ -1,10 +1,10 @@
 function AceitamosTampinhas() {
     return (
-        <section className=" relative max-w-full p-5  bg-laranja ">
+        <section className="max-w-full p-5 bg-laranja ">
             <div className="mb-5 text-3xl font-black text-barra-nav  lg:text-6xl lg:font-extrabold lg:p-5 lg:mx-20 ">
                 <h2>Aceitamos tampinhas de:</h2>
             </div>
-            <div className=" flex justify-center items-center  mx-15 my-2 text-1xl leading-7 font-normal text-marrom  bg-barra-nav border-2 border-marrom  border-dashed rounded-4xl sm:mx-60 lg:mx-120 lg:flex lg:justify-center  lg:leading-13 lg:text-4xl lg:p-13 lg:border-3">
+            <div className=" flex justify-items-start mx-14  text-1xl leading-7 font-normal text-marrom  bg-barra-nav border-2 border-marrom  border-dashed rounded-4xl sm:mx-60 lg:mx-120 lg:flex lg:justify-items-start  lg:leading-13 lg:text-4xl lg:p-13 lg:border-3">
                 <ul className="p-7" >
                     <li>Garrafas PET</li>
                     <li>Produtos de limpeza</li>
@@ -13,7 +13,7 @@ function AceitamosTampinhas() {
                     <li>Caixinhas longa vida</li>
                 </ul>
             </div>
-            <div className="absolute left-30 -right-5  top-19 sm:w-90 sm:left-100 lg:w-300 lg:-top-38 lg:left-81">
+            <div className="absolute -bottom-338 sm:w-90 sm:left-64 sm:top-566 lg:w-300 lg:top-365 lg:left-81">
                 <img className=" sm:max-w-full  lg:-top-210 lg:mr-100 lg:h-auto" src="/Gatopng.png" alt="Imagem gato"></img>
             </div>
         </section>
--- src/assets/components/JunteTampinhas/JunteTampinhas.jsx
+++ src/assets/components/JunteTampinhas/JunteTampinhas.jsx
@@ -5,12 +5,12 @@ function JunteTampinhas() {
         <section >
             <aside className="relative lg:flex lg:bg-rosa-claro">
                 {/*Textos*/}
-                <div className="absolute inset-0 flex flex-col items-center text-center pt-3 lg:static lg:w-[50%] lg:justify-center lg:items-start lg:text-left lg:px-8">
+                <div className="absolute inset-0 flex flex-col items-center text-center lg:static lg:w-[50%] lg:justify-center lg:items-start lg:text-left lg:px-8">
                     <h1 className="font-black text-5xl leading-13 p-5 lg:text-6xl">
                         Junte <span className="block lg:inline text-barra-nav lg:bg-laranja lg:rounded-[60px] lg:px-4 ">tampinhas,</span>
                         <span className="block pt-3 lg:leading-loose lg:items-start lg:text-left">salve vidas!</span>
                     </h1>
-                    <p className=" -my-4 lg:font-semibold lg:text-[25px] lg:ml-7">
+                    <p className=" -my-6  text-1xl lg:font-semibold lg:text-[25px] lg:ml-7">
                         Cada tampinha doada se<br />
                         transforma em alimento,<br />
                         medicamentos e amor para os,<br />
